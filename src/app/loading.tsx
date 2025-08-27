79a156bd0f7c15b489f9c46ba1c419214133a7e0
petehod <pete.hod1@gmail.com>
2025-08-27 15:24:27 -0500

    Add loading components and integrate react-spinners
    - Introduced Loading component to display a loading animation using PacmanLoader from react-spinners.
    - Created LoadingScreen components for general and album-specific loading states.
    - Updated package.json and package-lock.json to include react-spinners dependency for loading animations.

---
+++ src/app/loading.tsx
@@ -0,0 +1,5 @@
+import Loading from "@/components/Dumb/Loading/Loading";
+
+export default function LoadingScreen() {
+  return <Loading />;
+}
---
+++ src/components/Dumb/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+import { PacmanLoader } from "react-spinners";
+
+export default function Loading() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <PacmanLoader color="#fff" />
+    </main>
+  );
+}
